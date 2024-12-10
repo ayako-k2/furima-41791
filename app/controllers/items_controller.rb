946db13ea4549a17bdb7bb01a92fcffbab4561dd
ayako-kiyonari <a.kiyonari@gmail.com>
2024-12-10 16:39:12 +0900

    New & Create Action

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,11 +5,21 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
 
+  def create
+    @item = Item.new(item_params)
+    
+    if @item.save
+      redirect_to_root_path
+    else
+      render :new
+    end
+  end
 
-  # def message_params
-  #   params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
-  # end
-
+  private
+  def item_params
+    params.require(:item).permit(:item_name, :description, :category_id, :condition_id, :deliveryfee_id, :prefecture_id, :shipdate_id, :price, :image).merge(user_id: current_user.id)
+  end
 end
