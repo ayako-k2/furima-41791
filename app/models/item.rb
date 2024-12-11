7f9203be916f1f1e64a691200491d31c99f8d799
ayako-kiyonari <a.kiyonari@gmail.com>
2024-12-11 12:40:45 +0900

    Add pricecalc.js & Error Handling

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,10 +2,25 @@ class Item < ApplicationRecord
 
   has_one_attached :image
 
+  def was_attached?
+    self.image.attached?
+  end
+
+  validates :item_name, presence: {message:"can't be blank" }
+  validates :description, presence: {message:"can't be blank" }
+  validates :price, 
+             presence: true,
+             format: { with: /\A[0-9]+\z/, message: 'must be entered as a one-byte number and set within the range of ¥300 to ¥9,999,999.' }, 
+             numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+
   require 'active_hash'
   extend ActiveHash::Associations::ActiveRecordExtensions
    belongs_to :category
   
 
-  validates :category_id, :condition_id, :deliveryfee_id, :prefecture_id, :shipdate_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :category_id, :condition_id, :deliveryfee_id, :prefecture_id, :shipdate_id, numericality: { other_than: 1, message: "can't be blank" }, presence: true
+
 end
+
+
+
