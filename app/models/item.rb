92ce54d4d31d504e15ec53bdedc5a641fb7e5799
ayako-kiyonari <a.kiyonari@gmail.com>
2024-12-11 16:08:04 +0900

    Add Testcode

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,13 @@
 class Item < ApplicationRecord
 
-  has_one_attached :image
 
-  def was_attached?
-    self.image.attached?
+  has_one_attached :image
+  validate :image_presence
+
+  def image_presence
+    unless image.attached?
+     errors.add(:image, 'must be attached')
+    end
   end
 
   validates :item_name, presence: {message:"can't be blank" }
@@ -16,9 +20,14 @@ class Item < ApplicationRecord
   require 'active_hash'
   extend ActiveHash::Associations::ActiveRecordExtensions
    belongs_to :category
-  
+   belongs_to :condition
+   belongs_to :deliveryfee
+   belongs_to :prefecture
+   belongs_to :shipdate
 
   validates :category_id, :condition_id, :deliveryfee_id, :prefecture_id, :shipdate_id, numericality: { other_than: 1, message: "can't be blank" }, presence: true
+
+  belongs_to :user
 
 end
 
